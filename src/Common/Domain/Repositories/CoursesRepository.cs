cd5fb5e2492b0f5c0d88b6eb9e713b6df30d8b4d
Олег <somebody.jpg@gmail.com>
2016-04-18 13:20:34 +0300

    оформлена страница редактирования курсов

--- src/Common/Domain/Repositories/CoursesRepository.cs
+++ src/Common/Domain/Repositories/CoursesRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Domain.Entities;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 
 namespace Domain.Repositories
 {
@@ -8,6 +10,32 @@ namespace Domain.Repositories
         public CoursesRepository()
         {
             _dbContext = new CoursesContext();
+        }
+
+        public override void Update(int id, Course entity)
+        {
+            if(entity.Questions != null)
+            {
+                var context = (CoursesContext)_dbContext;
+                var existingCourse = context.Courses.FirstOrDefault(e => e.Id == id);
+                if(existingCourse != null)
+                {
+                    var questionsToRemove = new List<Question>();
+                    foreach(var q in context.Questions)
+                    {
+                        if (existingCourse.Questions.Contains(q))
+                        {
+                            questionsToRemove.Add(q);
+                        }
+                    }
+                    //var questionsToRemove = context.Questions
+                    //    .Where(q => existingCourse.Questions.Contains(q))
+                    //    .ToList();
+                    existingCourse.Questions.Clear();
+                }
+            }
+
+            base.Update(id, entity);
         }
 
         protected override DbSet<Course> GetEntityList()
