8eb2d0f8b9fef39ff2ea8efc43d51487194a8efa
Олег <somebody.jpg@gmail.com>
2016-04-19 13:01:29 +0300

    валидация пустого логина/пароля

--- src/Frontends/AdminFrontend/ViewModels/AuthViewModel.cs
+++ src/Frontends/AdminFrontend/ViewModels/AuthViewModel.cs
@@ -1,9 +1,15 @@
-﻿namespace AdminFrontend.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminFrontend.ViewModels
 {
     public class AuthViewModel
     {
         public string ReturnToUrl { get; set; }
+
+        [Required(ErrorMessage = "Поле Логин обязательно для заполнения")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "Поле Логин обязательно для заполнения")]
         public string Password { get; set; }
     }
 }
--- src/Frontends/WorkersFrontend/ViewModels/AuthViewModel.cs
+++ src/Frontends/WorkersFrontend/ViewModels/AuthViewModel.cs
@@ -1,9 +1,15 @@
-﻿namespace WorkersFrontend.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkersFrontend.ViewModels
 {
     public class AuthViewModel
     {
         public string ReturnToUrl { get; set; }
+
+        [Required(ErrorMessage = "Поле Логин обязательно для заполнения")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "Поле Логин обязательно для заполнения")]
         public string Password { get; set; }
     }
 }
