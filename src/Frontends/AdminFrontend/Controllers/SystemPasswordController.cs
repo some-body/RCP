8be0536f49a0316f0f72f6ed5ab4b9ca66125a59
Олег <somebody.jpg@gmail.com>
2016-04-19 13:47:17 +0300

    реализована возможность смены системного пароля

---
+++ src/Frontends/AdminFrontend/Controllers/SystemPasswordController.cs
@@ -0,0 +1,69 @@
+﻿using AdminFrontend.Auth;
+using AdminFrontend.ViewModels;
+using Distributed;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AdminFrontend.Controllers
+{
+    [RCPAuthorize(Roles = "Admin")]
+    public class SystemPasswordController : RCPController
+    {
+        private ApiQueryProvider<ICollection<SystemUser>, SystemUser> _usersQueryProvider;
+
+        public SystemPasswordController()
+        {
+            var backendUrl = Properties.Resources.SystemUsersBackendURL;
+            _usersQueryProvider = new ApiQueryProvider<ICollection<SystemUser>, SystemUser>(backendUrl, "SystemUsers");
+        }
+
+        public ActionResult Index()
+        {
+            return View(new ChangePasswordViewModel());
+        }
+
+        [HttpPost]
+        public JsonResult Save(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid || model.NewPassword != model.NewPasswordConfirm)
+            {
+                return Json(new
+                {
+                    success = false,
+                    msg = "Проверьте правильность введенных данных"
+                });
+            }
+
+            int adminId = 1;
+            try
+            {
+                var admin = new SystemUser
+                {
+                    Id = adminId,
+                    Login = null,
+                    Role = null,
+                    PasswordHash = model.NewPassword
+                };
+
+                var result = _usersQueryProvider.Post(admin);
+                return Json(new
+                {
+                    success = result.Success,
+                    msg = result.Success,
+                    returnTo = Url.Action("Index", "Home")
+                });
+            }
+            catch(Exception ex)
+            {
+                return Json(new
+                {
+                    success = false,
+                    msg = ex.GetBaseException().Message
+                });
+            }
+        }
+    }
+}
---
+++ src/Frontends/AdminFrontend/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdminFrontend.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessage = "Поле Текущий пароль обязательно для заполнения")]
+        [DisplayName("Текущий пароль")]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Поле Новый пароль обязательно для заполнения")]
+        [DisplayName("Новый пароль")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Поле Подтверждение обязательно для заполнения")]
+        [DisplayName("Подтверждение")]
+        public string NewPasswordConfirm { get; set; }
+    }
+}
